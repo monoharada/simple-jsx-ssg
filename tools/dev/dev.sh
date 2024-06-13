5cc102e148a3a9a933cbfef8e12a73775fd09111
monoharada <harada@monosus.co.jp>
2024-06-13 12:24:40 +0900

    fix:クロスプラットフォーム

--- tools/dev/dev.sh
+++ tools/dev/dev.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 # 使用中のポート8080を開放
-lsof -i :8080 | grep LISTEN | awk '{print $2}' | xargs kill -9
-
-
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    lsof -i :8080 | grep LISTEN | awk '{print $2}' | xargs kill -9
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    lsof -i :8080 | grep LISTEN | awk '{print $2}' | xargs kill -9
+elif [[ "$OSTYPE" == "msys" ]]; then
+    netstat -ano | findstr :8080 | findstr LISTENING | awk '{print $5}' | xargs taskkill /PID /F
+fi
 
 # bun run watcher.ts を実行
 BUILD_MODE=development bun run tools/dev/watcher.ts &
 
 # bunx reload を新しいポートで実行
 bun run tools/dev/bunserver.ts &
-# xdg-open http://localhost:3000/page-list # Linuxの場合
-open http://localhost:3000/page-list # macOSの場合
-# start http://localhost:3000/page-list # Windowsの場合
+
+# ブラウザを開く
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    xdg-open http://localhost:3000/page-list
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    open http://localhost:3000/page-list
+elif [[ "$OSTYPE" == "msys" ]]; then
+    start http://localhost:3000/page-list
+fi
